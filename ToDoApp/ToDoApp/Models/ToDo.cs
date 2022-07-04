ebf058bc629967137ef5e660cc197ecdf2cb1d2d
Michal Kašpárek <michal@ikasparek.cz>
2022-07-04 21:29:19 +0200

    Add project files.

---
+++ ToDoApp/ToDoApp/Models/ToDo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace ToDoApp.Models
+{
+    internal class ToDo : INotifyPropertyChanged
+    {
+        private string text;
+
+        public string Text
+        {
+            get => text;
+            set 
+            { 
+                text = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Text)));
+            }
+        }
+
+        public DateTime Date { get; set; }
+        public bool IsDone { get; set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
